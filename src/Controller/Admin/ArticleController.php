3bd9238ce08e28526698488c84f71d2b56797820
carime <azzougk5@gmail.com>
2020-10-06 12:31:52 +0200

    AJOUTE la gestion des articles
    Ajout d'un Crud complet des articles ainsi que de la publication
    d'articles

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use App\Form\ConfirmationType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,7 +24,7 @@ class ArticleController extends AbstractController
     public function index(ArticleRepository $repository)
     {
         return $this->render('admin/article/index.html.twig', [
-            'articles'=> $repository->findAll(),
+            'articles' => $repository->findAll(),
         ]);
     }
 
@@ -36,25 +37,25 @@ class ArticleController extends AbstractController
 
         //handleRequest permet au formulaire de récuperer les données POSt et de procéder a la validation
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             /**
              * getdata() permet de récupérer les données de formulaire
              * elle retourne par defaut un tableau des champ de formulaire
              * ou i retourne un objet de la classe a laquelle il est lié
              *
              */
-            /**@var Article $article*/
-           $article = $form->getData();
-           $em->persist($article);
-           $em->flush();
+            /**@var Article $article */
+            $article = $form->getData();
+            $em->persist($article);
+            $em->flush();
 
-           $this->addFlash('success','l\'articlea été créé');
-           return $this->redirectToRoute('admin_article_edit', [
-               'id' => $article->getID()
-           ]);
+            $this->addFlash('success', 'l\'articlea été créé');
+            return $this->redirectToRoute('admin_article_edit', [
+                'id' => $article->getID()
+            ]);
         }
-        return $this->render('admin/article/add.html.twig',[
-           'article_form' => $form->createView()
+        return $this->render('admin/article/add.html.twig', [
+            'article_form' => $form->createView()
         ]);
     }
 
@@ -73,7 +74,7 @@ class ArticleController extends AbstractController
         //le formulaire va directement modifier l'objet
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             /**
              * On a pas besoin d'appeler $form->getData()
              *     ->objet $article est directement modifié par le formulaire
@@ -84,9 +85,64 @@ class ArticleController extends AbstractController
             $em->flush();
             $this->addFlash('success', 'Article mis a jour.');
         }
-        return $this->render('admin/article/edit.html.twig',[
+        return $this->render('admin/article/edit.html.twig', [
             'article' => $article,
             'article_form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route ("/{id}/delete", name="delete")
+     */
+    public function delete(Article $article, Request $request, EntityManagerInterface $am)
+    {
+        $form = $this->createForm(ConfirmationType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $am->remove( $article);
+            $am->flush();
+
+            /**
+             * sprintf() sert a formater une chaine de caracteres
+             * le %s est un emplacement pour une chaine de caratères
+             */
+            $this->addFlash('info', sprintf('L article "%s" à été supprimée.', $article->getTitle()));
+            return $this->redirectToRoute('admin_article_list');
+        }
+
+        return $this->render('admin/article/delete.html.twig', [
+            'article' => $article,
+            'delete_form' =>$form->createView(),
+        ]);
+
+    }
+    /**
+     * @Route ("/{id}/publish/{token}", name="publish")
+     * le paramètre token servira a verifier que l'action a bien été demandé par
+     * l'administrateur connecté (protection contre les attaques CSRF)
+     */
+    public function publish(Article $article, string $token, EntityManagerInterface $em)
+    {
+
+        /**
+         * On doit nommer le jetons crsf
+         * symfony va comparer le jeton qu'il a enregistrer en sesion
+         * avec se que l'on a récupéré  dans l'adresse
+         */
+        if ($this->isCsrfTokenValid('article-publish', $token) === false) {
+            $this->addFlash('danger','le jeton est invalide.');
+            return $this->redirectToRoute('admin_article_edit', [
+                'id' => $article->getId(),
+            ]);
+        }
+
+        $article->setPublishedAt(new \DateTime());
+        $em->flush();
+
+        $this->addFlash('success', 'l article a été publié');
+        return $this->redirectToRoute('admin_article_edit', [
+            'id' => $article->getId(),
         ]);
     }
 }
